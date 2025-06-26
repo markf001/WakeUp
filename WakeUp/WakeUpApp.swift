99e302bc1484be0d9a0740f2a29899b77a7d7eff
markf001 <mark.fernandez@student.uts.edu.au>
2025-06-26 13:16:45 +1000

    Initial Commit

---
+++ WakeUp/WakeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpApp.swift
+//  WakeUp
+//
+//  Created by Mark Fernandez on 26/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
